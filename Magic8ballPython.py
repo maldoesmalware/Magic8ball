86b0cf10a3c7acdf1b28f4ea942655c29883038b
maldoesmalware <spencer.rick07@gmail.com>
2026-09-06 15:11:03 -0400

    Initial commit

---
+++ Magic8ballPython.py
@@ -0,0 +1,26 @@
+import random
+
+
+question = input('Question:    ')
+
+random_number = random.randint(1,9)
+
+if random_number == 1:
+    print ('indubitably')
+if random_number == 2:
+    print ('without a doubt')
+if random_number == 3:
+    print ('for certain')
+if random_number == 4:
+    print ('answer hazy, ask again later')
+if random_number == 5:
+    print ('ask again later')
+if random_number == 6:
+    print ('better not say')
+if random_number == 7:
+    print ('indubitably NOT')
+if random_number == 8:
+    print ('unfortunately no')
+if random_number == 9:
+    print ('probably not')
+
